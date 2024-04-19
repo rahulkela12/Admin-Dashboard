9d4c032a2db53ce30c7e34d63bcf1d0326b28ac0
rahul@LAPTOP-7O1THPFP.(none) <rahulkela1234@gmail.com>
2024-04-19 19:32:59 +0530

    responsive

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,19 +1,63 @@
-import {RiDashboardFill, RiShoppingBag3Fill,RiCoupon3Fill } from 'react-icons/ri';
-import { Link, useLocation,Location } from 'react-router-dom';
-import {IoIosPeople} from 'react-icons/io'
-import {AiFillFileText} from 'react-icons/ai'
+import { useEffect, useState } from 'react';
 import { IconType } from 'react-icons';
-import {FaChartPie,FaChartLine, FaChartBar,FaStopwatch,FaGamepad} from 'react-icons/fa'
+import { AiFillFileText } from 'react-icons/ai';
+import { FaChartBar, FaChartLine, FaChartPie, FaGamepad, FaStopwatch } from 'react-icons/fa';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { IoIosPeople } from 'react-icons/io';
+import { RiCoupon3Fill, RiDashboardFill, RiShoppingBag3Fill } from 'react-icons/ri';
+import { Link, Location, useLocation } from 'react-router-dom';
 
 const AdminSidebar = () => {
     const location = useLocation();
+    
+    const [showModal , setShowModal] = useState<boolean>(false);
+    const [phoneActive,setPhoneActive] = useState<boolean>(window.innerWidth < 1100);
+
+    const resizeHandler = ()=>{
+        setPhoneActive(window.innerWidth < 1100);
+    };
+
+    useEffect(() => {
+      window.addEventListener("resize",resizeHandler)
+    
+      return () => {
+        removeEventListener("resize",resizeHandler);
+      }
+    }, [])
+    
+
   return (
-    <aside>
+    <>
+    {
+        phoneActive && <button id="hamburger" onClick={() => setShowModal(true)}>
+            <HiMenuAlt4/>
+        </button>
+    }
+    <aside
+    style={
+        phoneActive
+          ? {
+              width: "20rem",
+              height: "100vh",
+              position: "fixed",
+              top: 0,
+              left: showModal ? "0" : "-20rem",
+              transition: "all 0.5s",
+            }
+          : {}
+      }
+    >
         <h2>Logo.</h2>
       <DivOne location={location}/>
       <DivTwo location={location}/>
       <DivThree location={location}/>
+      {phoneActive && (
+          <button id="close-sidebar" onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        )}
     </aside>
+    </>
   )
 }
 
